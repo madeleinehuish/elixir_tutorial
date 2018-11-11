18239e83ff92abc95f83c944842d7c735ebb2d97
Madeleine Huish <madeleinehuish@gmail.com>
2018-11-11 09:30:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains work from a Stephen Grider tutorial I've been working on with Elixir code.
---
+++ cards/lib/cards.ex
@@ -0,0 +1,33 @@
+defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    #
+    # List.flatten(cards)
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+end
