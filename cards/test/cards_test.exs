1d2373b7369b8e9273095ca90f86a92f2aa04a89
Madeleine Huish <madeleinehuish@gmail.com>
2018-11-16 07:14:22 -0800

    finished testing and doc examples

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,4 +2,15 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck makes 52 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck) #note there is an exteme unlikely possibility that same deck returned
+    #can also do refute deck == Cards.shuffle(deck)
+  end
+
 end
