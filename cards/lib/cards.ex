53d6e4d893eed41c243aa1b9cd810d2a5d3ff0d4
Madeleine Huish <madeleinehuish@gmail.com>
2018-11-16 07:03:57 -0800

    added testing and docs examples

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,14 @@
 defmodule Cards do
 
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  # to see the docs open doc/Cards.html
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -22,10 +31,32 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -36,11 +67,15 @@ defmodule Cards do
   end
 
   def load(filename) do
-
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
       {:error, _reason} -> "That file does not exist!"
     end
+  end
+
+  def create_hand(hand_size) do
+    #pipe operator calls function and pushes return value into next function, first parameter
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
   end
 
 end
